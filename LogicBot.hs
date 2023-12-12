85d6c951c952a668a04017502557ec0fe009e453
Azad Ellafi <arellafi@bu.edu>
2023-12-12 01:50:14 -0500

    First commit with Logic Bot and Validity Checker code

---
+++ LogicBot.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad (when, void)
+import UnliftIO.Concurrent
+
+import qualified Data.Text.IO as TIO
+import Discord
+import Discord.Types
+import qualified Discord.Requests as R
+
+import Data.Text
+import Data.Maybe
+
+import ValidityChecker
+
+main :: IO ()
+main = do
+    userFacingError <- runDiscord $ Discord.def
+             { discordToken = "MTE4MzQ5MDEyODY2MDkyMjQ5OA.GnRvjV.xO4QT_mBZzx5i_-V_C5C31uFVK_dZXVNNLLBko"
+             , discordOnEvent = eventHandler
+             , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
+             } -- if you see OnLog error, post in the discord / open an issue
+
+    TIO.putStrLn userFacingError
+    -- userFacingError is an unrecoverable error
+    -- put normal 'cleanup' code in discordOnEnd (see examples)
+
+eventHandler :: Event -> DiscordHandler ()
+eventHandler event = case event of
+    MessageCreate m -> when (isEval m && not (fromBot m)) $ do
+        let argText = stripEvalPrefix (messageContent m)
+            arg = parseArg (unpack argText)
+            validity = isMaybeValid arg
+            idnum = show $ userId $ messageAuthor m
+            mention = "<@"++ idnum ++ ">"
+         in do
+            case arg of
+                Just actualArg -> do
+			case maxVariables actualArg of
+			   True -> do
+				void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "man_facepalming")
+                                void $ restCall (R.CreateMessage (messageChannelId m) (pack ("<@"++ idnum ++ ">" ++ " Your argument has more variables than 16 variables. Use less please.")))
+			   False -> do 
+				if validity
+                                        then void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "white_check_mark")
+                                        else void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "x")
+                                threadDelay (2 * 10^6)
+                                void $ restCall (R.CreateMessage (messageChannelId m) (if validity then pack (mention ++ " The argument you gave is Valid.") else pack (mention ++ " The argument you gave is invalid.")))
+                Nothing -> do
+                   void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "man_facepalming")
+                   void $ restCall (R.CreateMessage (messageChannelId m) (pack ("<@"++ idnum ++ ">" ++ " The argument you gave isn't well-formed.")))
+    _ -> return ()
+
+
+-- Helper Functions
+fromBot :: Message -> Bool
+fromBot = userIsBot . messageAuthor
+
+isEval :: Message -> Bool
+isEval = ("eval" `isPrefixOf`) . messageContent
+
+isSuperSecret :: Message -> Bool
+isSuperSecret = ("supersecret shhh" `isInfixOf`) . messageContent
+
+stripEvalPrefix :: Text -> Text
+stripEvalPrefix = maybe "" id . stripPrefix "eval "
+
+maxVariables :: Arg -> Bool
+maxVariables arg = Prelude.length (getArgVariables arg) > 16
---
+++ ValidityChecker.hs
@@ -0,0 +1,143 @@
+module ValidityChecker where
+
+-- Imports
+
+import Text.Parsec
+import Text.Parsec.String
+import Text.Parsec.Expr
+import Text.Parsec.Token
+import Text.Parsec.Language
+import Data.List.Utils
+import Data.String.Utils
+import Data.List
+
+--- Data Types
+-- Data Type represting Propositions:
+data Prop = Var String | Con Bool | Uno Unop Prop | Duo Duop Prop Prop deriving Show
+
+-- Data Type representing Unary Operator Not:
+data Unop = Not deriving Show
+
+-- Data Type representing Binary Operators:
+data Duop = And | Or | Implies | Iff deriving Show
+
+-- Data Type representing an Argument, is a record type consisting of a List of Propositions as premises, and a single Proposition as a conclusion.
+data Arg = Arg {
+  premises :: [Prop],
+  conclusion :: Prop
+} deriving (Show)
+
+
+--- Defining Parsers
+-- Define our Language
+def = emptyDef{ commentStart = "{-"
+              , commentEnd = "-}"
+              , identStart = letter
+              , identLetter = alphaNum
+              , opStart = oneOf "~&||<->"
+              , opLetter = oneOf "~&||<->"
+              , reservedOpNames = ["~", "!", "¬", "&", "&&", "∧", "·", "||", "∨", "+", "->", "⇒", "=>", "⊃", "→", "<->", "↔", "=", "≡", "⇔"]
+              , reservedNames = ["true", "⊤", "1", "false", "⊥", "0", "nop", ",", "|=", "∴"]
+              }
+
+-- Parses Tokens
+TokenParser{ parens = m_parens
+           , identifier = m_identifier
+           , reservedOp = m_reservedOp
+           , reserved = m_reserved
+           , semiSep1 = m_semiSep1
+           , whiteSpace = m_whiteSpace } = makeTokenParser def
+
+-- Parses Propositions
+exprparser :: Parser Prop
+exprparser = buildExpressionParser table term <?> "proposition"
+table = [ [Prefix (m_reservedOp "~" >> return (Uno Not))]
+	, [Prefix (m_reservedOp "!" >> return (Uno Not))]
+	, [Prefix (m_reservedOp "¬" >> return (Uno Not))]
+	, [Infix (m_reservedOp "&&" >> return (Duo And)) AssocLeft]
+	, [Infix (m_reservedOp "&" >> return (Duo And)) AssocLeft]
+	, [Infix (m_reservedOp "∧" >> return (Duo And)) AssocLeft]
+	, [Infix (m_reservedOp "·" >> return (Duo And)) AssocLeft]
+	, [Infix (m_reservedOp "||" >> return (Duo Or)) AssocLeft]
+	, [Infix (m_reservedOp "∨" >> return (Duo Or)) AssocLeft]
+	, [Infix (m_reservedOp "+" >> return (Duo Or)) AssocLeft]
+	, [Infix (m_reservedOp "<->" >> return (Duo Iff)) AssocLeft]
+        , [Infix (m_reservedOp "↔" >> return (Duo Iff)) AssocLeft]
+        , [Infix (m_reservedOp "=" >> return (Duo Iff)) AssocLeft]
+        , [Infix (m_reservedOp "≡" >> return (Duo Iff)) AssocLeft]
+        , [Infix (m_reservedOp "⇔" >> return (Duo Iff)) AssocLeft]
+	, [Infix (m_reservedOp "->" >> return (Duo Implies)) AssocLeft]
+	, [Infix (m_reservedOp "⇒" >> return (Duo Implies)) AssocLeft]
+	, [Infix (m_reservedOp "=>" >> return (Duo Implies)) AssocLeft]
+	, [Infix (m_reservedOp "⊃" >> return (Duo Implies)) AssocLeft]
+        ]
+
+-- Various Booleans
+term = m_parens exprparser
+       <|> fmap Var m_identifier
+       <|> (m_reserved "true" >> return (Con True))
+       <|> (m_reserved "⊤" >> return (Con True))
+       <|> (m_reserved "1" >> return (Con True))
+       <|> (m_reserved "false" >> return (Con False))
+       <|> (m_reserved "⊥" >> return (Con False))
+       <|> (m_reserved "0" >> return (Con False))
+
+-- Parse the Argument
+argParser :: Parser (Maybe Arg)
+argParser = do
+  premList <- exprparser `sepBy` (char ',' >> spaces)
+  spaces
+  string "|="
+  spaces
+  conclusionProp <- exprparser
+  eof
+  return (Just (Arg premList conclusionProp))
+
+-- Maybe wrapper around Argument Parser, also checks for Variable count to limit Long Compute Times
+parseArg :: String -> Maybe Arg
+parseArg input = case parse argParser "" input of
+  Left _  -> Nothing
+  Right result -> result
+
+
+--- Code that evalutes Truth Values and Determines Validity
+-- Evaluate truth value of formulas
+evalProp :: [(String, Bool)] -> Prop -> Bool
+evalProp _ (Con b)       = b
+evalProp assignments (Var x) = case lookup x assignments of
+  Just value -> value
+  Nothing    -> error $ "Variable " ++ x ++ " not found in truth assignments"
+evalProp assignments (Uno Not p)       = not (evalProp assignments p)
+evalProp assignments (Duo And p q)     = evalProp assignments p && evalProp assignments q
+evalProp assignments (Duo Or p q)      = evalProp assignments p || evalProp assignments q
+evalProp assignments (Duo Implies p q) = not (evalProp assignments p) || evalProp assignments q
+evalProp assignments (Duo Iff p q)     = evalProp assignments p == evalProp assignments q
+
+-- Function to extract variables from a proposition
+getVariables :: Prop -> [String]
+getVariables (Var x)         = [x]
+getVariables (Con _)         = []
+getVariables (Uno _ p)       = getVariables p
+getVariables (Duo _ p1 p2)   = nub (getVariables p1 ++ getVariables p2)
+
+-- Function to extract all variables used in an argument
+getArgVariables :: Arg -> [String]
+getArgVariables arg = nub (concatMap getVariables (premises arg) ++ getVariables (conclusion arg))
+
+-- Generate all possible truth assignments for a list of variables
+generateTruthAssignments :: [String] -> [[(String, Bool)]]
+generateTruthAssignments variables = sequence [[(var, value) | value <- [True, False]] | var <- variables]
+
+-- Function to check if an argument is valid for all truth assignments
+isValid :: Arg -> Bool
+isValid arg = all (\assignment -> not (all (evalProp assignment) (premises arg)) || evalProp assignment (conclusion arg)) truthAssignments
+  where
+    -- Generate all possible truth assignments
+    variables = nub $ concatMap getVariables (premises arg) ++ getVariables (conclusion arg)
+    truthAssignments = generateTruthAssignments variables
+
+-- Maybe wrapper on IsValid to catch errors
+isMaybeValid :: Maybe Arg -> Bool
+isMaybeValid marg = case marg of
+    Nothing -> False
+    Just arg -> isValid arg
